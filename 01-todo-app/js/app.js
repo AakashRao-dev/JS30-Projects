f3b53d1f96a985cf793e996d9aef294f7923812f
Aakash <aakash.codes02@gmail.com>
2023-02-02 20:41:33 +0530

    playing with DOM & JS

--- 01-todo-app/js/app.js
+++ 01-todo-app/js/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const themeContainer = document.querySelector('.theme__container');
+
+let toggleThemeEl = '';
+
+for (let i = 0; i < 3; i++) {
+  toggleThemeEl += `
+    <div class="theme__input">
+      <div class="toggle">
+        <div class="indicator"></div>
+      </div>
+    </div>
+`;
+}
+
+themeContainer.innerHTML += toggleThemeEl;
+
+const toggle = document.querySelectorAll('.toggle');
+
+toggle.forEach(toggle => {
+  toggle.addEventListener('click', function () {
+    toggle.classList.toggle('active');
+    // It will toggle {add/remove} active class from body
+    console.log('WOrking!');
+  });
+});
